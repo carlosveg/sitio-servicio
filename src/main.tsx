a99dc6064ac5a53bbb9d61e7b865bc43733b78be
carlos vega <carlosvegapro@gmail.com>
2024-01-23 13:10:22 -0600

    Intento 1 router

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,14 @@ import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
 import './index.css'
-import { HashRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 )
